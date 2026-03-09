118f7070cb7aaebd2f128300f9277e30fb5fa633
adafolabi <afolabiwale262@gmail.com>
2026-03-09 10:33:16 +0000

    v1.0

--- main.go
+++ main.go
@@ -1,21 +1,42 @@
 package main
 
 import (
+	"Ascii-Art/helperFunctions"
 	"fmt"
+	"log"
 	"os"
-	 "strings"
+	"strings"
 )
 
 func main() {
-	data, err := os.ReadFile("standard.txt")
 
-	if err != nil {
-		panic(err)
+	inputText := os.Args[1]
+	//fmt.Println(inputText)
+
+	inputText = strings.ReplaceAll(inputText, "\\n", "\n")
+	//fmt.Println(inputText)
+
+	if inputText == "" {
+		return
+	} else {
+		buffer, err := os.ReadFile("standard.txt")
+
+		if err != nil {
+			log.Fatal("Error Reading the banner file..")
+		}
+
+		banner := strings.Split(string(buffer), "\n")
+		//fmt.Println(banner)
+
+		lines := strings.Split(inputText, "\n")
+		//fmt.Println(len(lines))
+
+		for _, line := range lines {
+			if line == "" {
+				fmt.Println()
+				continue
+			}
+			helperFunctions.PrintAsciiArt(line, banner)
+		}
 	}
-
-	text := string(data)
-
-	visible := strings.ReplaceAll(text, " ", ".")
-
-	fmt.Println(visible)
 }
